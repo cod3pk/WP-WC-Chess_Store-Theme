907607929626035c4463e8fa098f1fd180cf8885
Asfand Yar ALi Khan <asfandkhan404@gmail.com>
2022-06-30 19:27:34 +0500

    Changes in Product Title

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -28,7 +28,7 @@ if (empty($product) || !$product->is_visible()) {
 
 		</a>
 		<div class="product-content-wrapper" >
-			<p class="mb-2 item-desc-1 black-links"><?php echo wp_trim_words($product->name, 5); ?></p>
+			<p class="mb-2 item-desc-1 black-links"><?php echo $product->name; ?></p>
 			<p class="mb-2 item-price"><?php echo get_woocommerce_currency_symbol() . $product_data->get_price(); ?></p>
 		</div>
 		<p class="mb-5 item-desc-2 mbl-none-products"><?php echo wp_trim_words($product_data->get_description(), 15) ?></p>
