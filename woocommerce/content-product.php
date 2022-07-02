9b6c0a335eea3a5b725b6087e3aedddb0c1b67b6
Asfand Yar ALi Khan <asfandkhan404@gmail.com>
2022-07-02 13:35:50 +0500

    Revised product cards

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -22,24 +22,38 @@ if (empty($product) || !$product->is_visible()) {
 	?>
 
 	<div class="chess-items">
+
 		<a href="<?php echo get_permalink($product->id) ?>" class="text-decoration-none">
-
 			<img class="mb-3 img-fluid chess-item-img" src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($product->id), 'single-post-thumbnail')[0]; ?>" alt="<?php echo $product->name; ?>">
-
 		</a>
-		<div class="product-content-wrapper" >
-			<p class="mb-2 item-desc-1 black-links product-cards-title"><?php echo $product->name; ?></p>
-			<p class="mb-2 item-price"><?php echo get_woocommerce_currency_symbol() . $product_data->get_price(); ?></p>
-		</div>
-		<p class="mb-5 item-desc-2 mbl-none-products"><?php echo wp_trim_words($product_data->get_description(), 15) ?></p>
+
+		<a href="<?php echo get_permalink($product->id) ?>" class="text-decoration-none">
+			<div class="product-content-wrapper">
+				<p class="mb-2 item-desc-1 black-links product-cards-title">
+					<?php echo $product->name; ?>
+				</p>
+				<p class="mb-2 item-price">
+					<?php echo get_woocommerce_currency_symbol() . $product_data->get_price(); ?>
+				</p>
+			</div>
+		</a>
+
+		<a href="<?php echo get_permalink($product->id) ?>" class="text-decoration-none">
+			<p class="mb-5 item-desc-2 mbl-none-products">
+				<?php $custom_product_excerpt = get_post_meta($post->ID, '_chess-store_custom_product_excerpt_wysiwyg', true); ?>
+				<?php echo $custom_product_excerpt; ?>
+			</p>
+		</a>
+
 		<div class="text-center">
 			<form action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>" method="post" enctype='multipart/form-data'>
 				<?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
-				<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="black-links chess-item-btn ajax_add_to_cart text-decoration-none"><?php echo __( 'Buy', 'chess-store' ); ?></button>
+				<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="black-links chess-item-btn ajax_add_to_cart text-decoration-none"><?php echo __('Buy', 'chess-store'); ?></button>
 
 				<?php do_action('woocommerce_after_add_to_cart_button'); ?>
 			</form>
 		</div>
+
 	</div>
 </div>
