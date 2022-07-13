8a2673ff026cb865ccdbbc9676ec6b7dee456561
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2022-07-13 13:42:26 +0500

    Versioning changes

--- functions.php
+++ functions.php
@@ -8,9 +8,9 @@
  * @package chess_store
  */
 
-if ( !defined( '_S_VERSION' ) ) {
+if ( !defined( '_CHESS_STORE_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( '_CHESS_STORE_VERSION', '1.0.0' );
 }
 
 /**
