9acd8b166461adbe937c4b9215ea5041e5dccc5c
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2022-07-21 15:08:11 +0500

    Related Products refactoring

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -3,45 +3,37 @@
 /**
  * Related Products
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product/related.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
  * @see         https://docs.woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
  * @version     3.9.0
  */
 
-if (!defined('ABSPATH')) {
+if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ($related_products) : ?>
+if ( count( $related_products ) == 4 ) : ?>
 
 	<section class="related products">
 
 		<?php
-		$heading = apply_filters('woocommerce_product_related_products_heading', __('Similar Products', 'chess-store'));
+		$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Similar Products', 'chess-store' ) );
 
-		if ($heading) :
+		if ( $heading ) :
 		?>
-			<h2><?php echo esc_html($heading); ?></h2>
+			<h2><?php echo esc_html( $heading ); ?></h2>
 		<?php endif; ?>
 
 		<section class="chess-price">
 			<div class="row">
-				<?php foreach ($related_products as $related_product) : ?>
+				<?php foreach ( $related_products as $related_product ) : ?>
 
 					<?php
-					$post_object = get_post($related_product->get_id());
+					$post_object = get_post( $related_product->get_id(  ) );
 
-					setup_postdata($GLOBALS['post'] = &$post_object); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
+					setup_postdata( $GLOBALS[ 'post' ] = &$post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
 
-					wc_get_template_part('content', 'product');
+					wc_get_template_part( 'content', 'product' );
 					?>
 
 				<?php endforeach; ?>
@@ -50,6 +42,56 @@ if ($related_products) : ?>
 
 	</section>
 <?php
-endif;
 
-wp_reset_postdata();
+else:
+
+global $product;
+$product_cat_id = $product->category_ids[ 0 ];
+$child_term = get_term( $product_cat_id, 'product_cat' );
+
+if ( $child_term->parent > 0 ){
+	$parent_term = get_term( $child_term->parent, 'product_cat' );
+
+	$parent_products = new WP_Query( [
+		'post_type'			=> 'product',
+		'posts_per_page'	=> 4,
+		'term_id'			=> $child_term->parent
+	] );
+
+	$related_products = $parent_products->posts;
+	?>
+
+	<section class="related products">
+
+	<?php
+	$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Similar Products', 'chess-store' ) );
+
+	if ( $heading ) :
+	?>
+		<h2><?php echo esc_html( $heading ); ?></h2>
+	<?php endif; ?>
+
+	<section class="chess-price">
+		<div class="row">
+			<?php foreach ( $related_products as $related_product ) : ?>
+
+				<?php
+				$post_object = get_post( $related_product->get_id(  ) );
+
+				setup_postdata( $GLOBALS[ 'post' ] = &$post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
+
+				wc_get_template_part( 'content', 'product' );
+				?>
+
+			<?php endforeach; ?>
+		</div>
+	</section>
+
+	</section>
+
+	<?php
+	wp_reset_postdata(  );
+
+}
+
+endif;
