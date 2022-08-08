affae4086d672f6081aa62114e2531fda212ca46
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2022-08-08 22:58:23 +0500

    Header refactoring

--- header.php
+++ header.php
@@ -12,154 +12,165 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-<section class="nav-top-section nav-top-desktop container-fluid">
+    <section class="nav-top-section nav-top-desktop container-fluid">
 
-    <!-- Topbar -->
-    <div class="nav-top-content d-flex justify-content-between">
-        <div class="nav-top-left-side d-flex justify-content-between">
-            <div class="nav-top-social-links d-flex justify-content-between">
+        <!-- Topbar -->
+        <div class="nav-top-content d-flex justify-content-between">
+            <div class="nav-top-left-side d-flex justify-content-between">
+                <div class="nav-top-social-links d-flex justify-content-between">
 
-				<?php if ( get_theme_mod( 'topbar_options_insta' ) ) : ?>
-                    <a href="<?php echo get_theme_mod( 'topbar_options_insta' ); ?>" target="_blank">
-                        <img src="<?php echo wp_get_attachment_image_src( 81 )[ 0 ] ?>" alt="Instagram">
-                    </a>
-				<?php endif; ?>
+                    <?php if (get_theme_mod('topbar_options_insta')) : ?>
+                        <a href="<?php echo get_theme_mod('topbar_options_insta'); ?>" target="_blank">
+                            <img src="<?php echo wp_get_attachment_image_src(81)[0] ?>" alt="Instagram">
+                        </a>
+                    <?php endif; ?>
 
-				<?php if ( get_theme_mod( 'topbar_options_facebook' ) ) : ?>
-                    <a href="<?php echo get_theme_mod( 'topbar_options_facebook' ); ?>" target="_blank">
-                        <img src="<?php echo wp_get_attachment_image_src( 78 )[ 0 ] ?>" alt="Facebook">
-                    </a>
-				<?php endif; ?>
+                    <?php if (get_theme_mod('topbar_options_facebook')) : ?>
+                        <a href="<?php echo get_theme_mod('topbar_options_facebook'); ?>" target="_blank">
+                            <img src="<?php echo wp_get_attachment_image_src(78)[0] ?>" alt="Facebook">
+                        </a>
+                    <?php endif; ?>
 
-				<?php if ( get_theme_mod( 'topbar_options_whatsapp' ) ) : ?>
-                    <a href="https://wa.me/<?php echo get_theme_mod( 'topbar_options_whatsapp' ); ?>" target="_blank">
-                        <img src="<?php echo wp_get_attachment_image_src( 102 )[ 0 ] ?>" alt="Whatsapp">
-                    </a>
-				<?php endif; ?>
+                    <?php if (get_theme_mod('topbar_options_whatsapp')) : ?>
+                        <a href="https://wa.me/<?php echo get_theme_mod('topbar_options_whatsapp'); ?>" target="_blank">
+                            <img src="<?php echo wp_get_attachment_image_src(102)[0] ?>" alt="Whatsapp">
+                        </a>
+                    <?php endif; ?>
 
+                </div>
+                <div class="nav-top-num d-flex justify-content-between">
+                    <?php if (get_theme_mod('topbar_options_phone_one')) : ?>
+                        <a href="tel:<?php echo get_theme_mod('topbar_options_phone_one'); ?>" class="white-links text-decoration-none white-links phone-numbers" target="_blank">
+                            <p class="mb-0 phone-numbers"><?php echo get_theme_mod('topbar_options_phone_one'); ?></p>
+                        </a>
+                    <?php endif; ?>
+
+                    <?php if (get_theme_mod('topbar_options_phone_two')) : ?>
+                        <a href="tel:<?php echo get_theme_mod('topbar_options_phone_two'); ?>" class="white-links text-decoration-none white-links" target="_blank">
+                            <p class="mb-0 phone-numbers"><?php echo get_theme_mod('topbar_options_phone_two'); ?></p>
+                        </a>
+                    <?php endif; ?>
+                </div>
             </div>
-            <div class="nav-top-num d-flex justify-content-between">
-				<?php if ( get_theme_mod( 'topbar_options_phone_one' ) ) : ?>
-                    <a href="tel:<?php echo get_theme_mod( 'topbar_options_phone_one' ); ?>"
-                       class="white-links text-decoration-none white-links phone-numbers" target="_blank">
-                        <p class="mb-0 phone-numbers"><?php echo get_theme_mod( 'topbar_options_phone_one' ); ?></p>
+            <div class="nav-flag hide-on-mobile">
+                <?php get_sidebar('topbar'); ?>
+            </div>
+            <div class="nav-flag hide-on-desktop">
+                <!-- Mobile Topbar elements -->
+                <ul class="ils-wrapper ul pe-3 flx-order-3 d-flex align-items-center mb-0 ms-0 ps-0">
+                    <a class=" text-decoration-none " href="<?php echo wc_get_cart_url(); ?>">
+                        <img class="shopping-cart align-bottom" src="<?php echo get_template_directory_uri() . '/images/cart.png' ?>" alt="Cart">
                     </a>
-				<?php endif; ?>
-
-				<?php if ( get_theme_mod( 'topbar_options_phone_two' ) ) : ?>
-                    <a href="tel:<?php echo get_theme_mod( 'topbar_options_phone_two' ); ?>"
-                       class="white-links text-decoration-none white-links" target="_blank">
-                        <p class="mb-0 phone-numbers" ><?php echo get_theme_mod( 'topbar_options_phone_two' ); ?></p>
-                    </a>
-				<?php endif; ?>
+                    <span class="vertical-line ps-3">|</span>
+                    <span class="nav-ils-text fs-4">ILS</span>
+                </ul>
             </div>
         </div>
-        <div class="nav-flag">
-            <?php get_sidebar( 'topbar' ); ?>
+        <!-- End Topbar -->
+
+        <!-- Header -->
+        <div class="nav-top-section-mobile nav-top-mobile container-fluid">
+            <div class="nav-top-mobile-content d-flex justify-content-between">
+                <div class="nav-mobile-cart d-flex align-items-center">
+                    <ul class="ils-wrapper ul pe-3 flx-order-3 d-flex align-items-center mb-0 ms-0 ps-0">
+                        <a class=" text-decoration-none " href="<?php echo wc_get_cart_url(); ?>">
+                            <img class="shopping-cart align-bottom" src="<?php echo get_template_directory_uri() . '/images/cart.png' ?>" alt="Cart">
+                        </a>
+                        <span class="vertical-line ps-3">|</span>
+                        <span class="nav-ils-text fs-4">ILS</span>
+                    </ul>
+                </div>
+                <!-- <span class="top-nav-ver-line"></span> -->
+                <div class="nav-mobile-social-links d-flex align-items-center">
+
+                    <?php if (get_theme_mod('topbar_options_insta')) : ?>
+                        <a href="<?php echo get_theme_mod('topbar_options_insta'); ?>" target="_blank">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/instagram.png ?>" alt="Instagram">
+                        </a>
+                    <?php endif; ?>
+
+                    <?php if (get_theme_mod('topbar_options_facebook')) : ?>
+                        <a href="<?php echo get_theme_mod('topbar_options_facebook'); ?>" target="_blank">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/facebook.png ?>" alt="Facebook">
+                        </a>
+                    <?php endif; ?>
+
+                    <?php if (get_theme_mod('topbar_options_whatsapp')) : ?>
+                        <a href="https://wa.me/<?php echo get_theme_mod('topbar_options_whatsapp'); ?>" target="_blank" class="header-whatsapp-link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/whatsapp.png ?>" alt="Whatsapp">
+                        </a>
+                    <?php endif; ?>
+
+                    <div class="top-nav-line"></div>
+                    <?php if (get_theme_mod('topbar_options_phone_one')) : ?>
+                        <a class="telephone-img" href="tel:<?php echo get_theme_mod('topbar_options_phone_one'); ?>">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/phone.png ?>" alt="Phone">
+                        </a>
+                    <?php endif; ?>
+                </div>
+            </div>
         </div>
-    </div>
-    <!-- End Topbar -->
+        <!-- End Header -->
+
+    </section>
+
 
     <!-- Header -->
-    <div class="nav-top-section-mobile nav-top-mobile container-fluid">
-        <div class="nav-top-mobile-content d-flex justify-content-between">
-            <div class="nav-mobile-cart d-flex align-items-center">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/iflag.png ?>">
-            </div>
-            <!-- <span class="top-nav-ver-line"></span> -->
-            <div class="nav-mobile-social-links d-flex align-items-center">
+    <!-- Desktop -->
+    <nav class="nav desktop-nav">
+        <div class="container-fluid nav justify-content-between align-items-center py-2">
+            <ul class="ul flx-order-2 nav-logo-wrapper ms-0">
+                <?php the_custom_logo(); ?>
+            </ul>
+            <?php
 
-				<?php if ( get_theme_mod( 'topbar_options_insta' ) ) : ?>
-                    <a href="<?php echo get_theme_mod( 'topbar_options_insta' ); ?>" target="_blank">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/instagram.png ?>" alt="Instagram">
-                    </a>
-				<?php endif; ?>
+            wp_nav_menu(array(
+                'theme_location'    => 'header',
+                'depth'             => 3,
+                'container'         => 'div',
+                'menu_class'        => 'menu',
+                'menu_id'           => 'header',
+                'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker(),
+            ));
+            ?>
 
-				<?php if ( get_theme_mod( 'topbar_options_facebook' ) ) : ?>
-                    <a href="<?php echo get_theme_mod( 'topbar_options_facebook' ); ?>" target="_blank">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/facebook.png ?>" alt="Facebook">
-                    </a>
-				<?php endif; ?>
+            <ul class="hamburger mb-0 ms-0 ps-0">
+                <button class="hamburger-btn"><img src="<?php echo get_template_directory_uri(); ?>/images/hamburger.png ?>" alt="Menu">
+                </button>
+            </ul>
 
-				<?php if ( get_theme_mod( 'topbar_options_whatsapp' ) ) : ?>
-                    <a href="https://wa.me/<?php echo get_theme_mod( 'topbar_options_whatsapp' ); ?>" target="_blank" class ="header-whatsapp-link">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/whatsapp.png ?>" alt="Whatsapp">
-                    </a>
-				<?php endif; ?>
+            <ul class="ils-wrapper ul pe-3 flx-order-3 d-flex align-items-center mb-0 ms-0 ps-0">
 
-                <div class="top-nav-line"></div>
-				<?php if ( get_theme_mod( 'topbar_options_phone_one' ) ) : ?>
-                    <a class="telephone-img" href="tel:<?php echo get_theme_mod( 'topbar_options_phone_one' ); ?>">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/phone.png ?>" alt="Phone">
+                <?php if (WC()->cart->get_cart_contents_count() != 0) : ?>
+                    <a class=" text-decoration-none " href="<?php echo wc_get_cart_url(); ?>">
+                        <img class="shopping-cart pe-3 align-bottom" src="<?php echo wp_get_attachment_image_src(75)[0] ?>" alt="Cart">
                     </a>
-				<?php endif; ?>
-            </div>
+                    <a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e('View your shopping cart'); ?>">
+                        <?php echo sprintf(_n('%d item', '%d items', WC()->cart->get_cart_contents_count()), WC()->cart->get_cart_contents_count()); ?>
+                        –
+                        <?php echo WC()->cart->get_cart_total(); ?>
+                    </a>
+                    <span class="vertical-line ms-2">|</span>
+                    <span class="nav-ils-text fs-4">ILS</span>
+                <?php else : ?>
+                    <a class=" text-decoration-none " href="<?php echo wc_get_cart_url(); ?>">
+                        <img class="shopping-cart align-bottom" src="<?php echo wp_get_attachment_image_src(75)[0] ?>" alt="Cart">
+                    </a>
+                    <span class="vertical-line ps-3">|</span>
+                    <span class="nav-ils-text fs-4">ILS</span>
+                <?php endif; ?>
+
+            </ul>
         </div>
-    </div>
-    <!-- End Header -->
-
-</section>
-
-
-<!-- Header -->
-<!-- Desktop -->
-<nav class="nav desktop-nav">
-    <div class="container-fluid nav justify-content-between align-items-center py-2">
-        <ul class="ul flx-order-2 nav-logo-wrapper ms-0">
-			<?php the_custom_logo(); ?>
-        </ul>
-		<?php
-
-		wp_nav_menu( array(
-			'theme_location'    => 'header',
-			'depth'             => 3,
-			'container'         => 'div',
-			'menu_class'        => 'menu',
-			'menu_id'           => 'header',
-			'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-			'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-			'walker'            => new WP_Bootstrap_Navwalker(),
-		) );
-		?>
-
-        <ul class="hamburger mb-0 ms-0 ps-0">
-            <button class="hamburger-btn"><img src="<?php echo get_template_directory_uri(); ?>/images/hamburger.png ?>" alt="Menu">
-            </button>
-        </ul>
-
-        <ul class="ils-wrapper ul pe-3 flx-order-3 d-flex align-items-center mb-0 ms-0 ps-0">
-
-			<?php if ( WC()->cart->get_cart_contents_count() != 0 ) : ?>
-                <a class=" text-decoration-none " href="<?php echo wc_get_cart_url(); ?>">
-                    <img class="shopping-cart pe-3 align-bottom"
-                         src="<?php echo wp_get_attachment_image_src( 75 )[ 0 ] ?>" alt="Cart">
-                </a>
-                <a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>"
-                   title="<?php _e( 'View your shopping cart' ); ?>">
-					<?php echo sprintf( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?>
-                    –
-					<?php echo WC()->cart->get_cart_total(); ?>
-                </a>
-                <span class="vertical-line ms-2">|</span>
-                <span class="nav-ils-text fs-4">ILS</span>
-			<?php else: ?>
-                <a class=" text-decoration-none " href="<?php echo wc_get_cart_url(); ?>">
-                    <img class="shopping-cart align-bottom"
-                         src="<?php echo wp_get_attachment_image_src( 75 )[ 0 ] ?>" alt="Cart">
-                </a>
-                <span class="vertical-line ps-3">|</span>
-                <span class="nav-ils-text fs-4">ILS</span>
-			<?php endif; ?>
-
-        </ul>
-    </div>
-</nav>
+    </nav>
